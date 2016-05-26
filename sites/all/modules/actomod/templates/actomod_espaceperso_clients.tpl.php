7269b138c00de8b08ec7c84dee8e4830d6018d65
Camille Castillo <camillecastillo@Alexiss-iMac.local>
2016-05-26 14:02:25 +0200

    wip

--- sites/all/modules/actomod/templates/actomod_espaceperso_clients.tpl.php
+++ sites/all/modules/actomod/templates/actomod_espaceperso_clients.tpl.php
@@ -3,25 +3,54 @@
    
 ?>
 
-<div>
-   LOGIN
-     <?php print drupal_render($loginform); ?>
-    
-    
-    
-</div>
-<div >
-   REGISTER
+<div class="c-block-contact"></div>
 
-   <?php
-        print render($registerform["field_lastname"]);
-        $registerform["account"]["name"]["#description"] = "qsd";
-            
-    ?>
-      <?php dpm($registerform); ?>    
-   <hr/>
+<div class="c-form">
     
-    
-    <?php print drupal_render($registerform); ?>
+    <div class="cc-block">
+
+        <div class="c-block">
+
+            <div class="c-gradient">
+
+               <div class="form-col">
+
+                    <div>
+                       <h4>LOGIN</h4>
+                         <?php print drupal_render($loginform); ?>
+                    </div>
+
+               </div>
+
+                <div class="form-col">
+
+                    <div >
+                       <h4>REGISTER</h4>
+
+                       <?php
+                            print render($registerform["field_lastname"]);
+                            $registerform["account"]["name"]["#description"] = "qsd";
+                        ?>
+                          <?php dpm($registerform); ?>
+                       <hr/>
+                        <?php print drupal_render($registerform); ?>
+
+                    </div>
+
+                </div>
+
+                <div class="clear">
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
     
 </div>
+
+
+
