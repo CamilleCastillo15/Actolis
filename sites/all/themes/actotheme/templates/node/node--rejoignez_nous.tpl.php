04b5ac103ed1340a50f787bacc4b7bb25a26f80a
Camille Castillo <camillecastillo@Alexiss-iMac.local>
2016-06-09 16:30:44 +0200

    wip : les valeurs clés sont déplacées vers la page "Espace talents"

--- sites/all/themes/actotheme/templates/node/node--rejoignez_nous.tpl.php
+++ sites/all/themes/actotheme/templates/node/node--rejoignez_nous.tpl.php
@@ -20,6 +20,16 @@
 
     drupal_add_css('.rejoignez_nous .c-block-histoire .c-img {background-image: url("'.$p.'") !important; } ', 'inline');
 
+    $fc_phrases_valeurs = field_get_items('node', $node, 'field_fc_phrases_valeurs');
+
+    if($fc_phrases_valeurs){
+        $idti_phrases_valeurs = array();
+        foreach ($fc_phrases_valeurs as $fc_field) {
+            $idti_phrases_valeurs[] = $fc_field['value'];
+        }
+        $collectionsg_phrases_valeurs =  field_collection_item_load_multiple($idti_phrases_valeurs);
+    }
+
 ?>
 
 <div class="rejoignez_nous">
@@ -40,6 +50,81 @@
 
         </div>
     
+    </div>
+
+        <div class="c-header phrases-valeurs">
+
+        <div class="dark-layer">
+
+            <div class="c-phrases-container">
+
+                <?php
+
+                    for($i = 0; $i < 2; $i++) {
+
+                    $collectiong_2_phrases_valeurs = $collectionsg_phrases_valeurs[$idti_phrases_valeurs[$i]];
+
+                    $phrase = field_view_field('field_collection_item', $collectiong_2_phrases_valeurs, 'field_phrase');
+
+                    $valeur = field_view_field('field_collection_item', $collectiong_2_phrases_valeurs, 'field_valeur');
+
+                ?>
+
+                <div class="c-in">
+                    <div class="c-picto">
+                        <h2 class="phrase"><?php print render($phrase); ?></h2>
+                        <h2 class="valeur"><?php print render($valeur); ?></h2>
+                    </div>
+                </div>
+
+                <?php } ?>
+
+            </div>
+
+            <div class="c-phrases-container">
+
+                <?php
+
+                for($i = 2; $i < 4; $i++) {
+
+                    $collectiong_2_phrases_valeurs = $collectionsg_phrases_valeurs[$idti_phrases_valeurs[$i]];
+
+                    $phrase = field_view_field('field_collection_item', $collectiong_2_phrases_valeurs, 'field_phrase');
+
+                    $valeur = field_view_field('field_collection_item', $collectiong_2_phrases_valeurs, 'field_valeur');
+
+                ?>
+                <div class="c-in">
+                <div class="c-picto">
+                    <h2 class="phrase"><?php print render($phrase); ?></h2>
+                    <h2 class="valeur"><?php print render($valeur); ?></h2>
+                </div>
+                </div>
+
+
+                <?php } ?>
+
+            </div>
+
+            <div class="c-phrases-container phrases-container-2">
+
+            <?php
+
+                    $valeur_cle = field_view_field("node", $node, 'field_valeur_cle');
+
+                    $phrase_valeur_cle = field_view_field("node", $node, 'field_phrase_valeur_cle');
+
+            ?>
+
+                <div class="c-picto c-picto-cle">
+                    <h2 class="phrase phrase_cle"><?php print render($phrase_valeur_cle); ?></h2>
+                    <h2 class="valeur valeur_cle"><?php print render($valeur_cle); ?></h2>
+                </div>
+
+            </div>
+
+        </div>
+
     </div>
 
     <div class="c-block-offres">
