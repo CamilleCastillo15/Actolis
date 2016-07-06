6f84c34d47b7d3bb640e84c01648ad86492efee4
Camille Castillo <camillecastillo@Alexiss-iMac.local>
2016-07-06 12:28:49 +0200

    wip : fonctionnement de la barre des réglementations

--- sites/all/themes/actotheme/js/actotheme.domaine.js
+++ sites/all/themes/actotheme/js/actotheme.domaine.js
@@ -8,13 +8,10 @@
                 $(".block-all").css("height",$(this).height());
             }else{
                 $(".block-all").css("height","auto");
-
             }
-
         });
 
         var swiper = new Swiper('.swiper-container', {
-
             spaceBetween: 0,
             autoplay : 5000,
             autoHeight:true,
@@ -22,14 +19,10 @@
             paginationClickable: true,
             nextButton: '.swiper-button-next',
             prevButton: '.swiper-button-prev'
-
         });
-
-//        console.log("swiperdomaine");
 
         $(window).bind('scroll', function() {
             var scrollTop = $(window).scrollTop();
-            console.log(scrollTop);
 
             if(scrollTop > 498){
                 $('.frise-actions').addClass('fixed');
@@ -42,38 +35,84 @@
         $('.frise-actions .titre-actions').click(function(){
 
             event.preventDefault();
-//            console.log("nav-actions");
 
             $(".active").removeClass("active");
-//            console.log(this);
-//
             var divClosest = $(this).closest('div');
-//            console.log(divClosest);
 
             var classes = divClosest.attr('class');
             var theClass = classes.split(' ')[1];
 
-            $(this).closest('div').addClass("active");
-//
-//            var theClass = $(this).attr("class");
-//            console.log(theClass);
+//            $(this).closest('div').addClass("active");
 
             var scrollDiv = $(this).attr('href');
-//            console.log(scrollDiv);
 
-//            console.log($('.'+scrollDiv));
-//
-//            $('.'+theClass).parent('li').addClass('active');
-//            //Animate
             $('html, body').stop().animate({
                 scrollTop: $('.'+theClass+'.liens').offset().top - 160
             }, 400);
-//            return false;
           });
-//          $('.scrollTop a').scrollTop();
 
-}
+var aItems = $('.c-text .title.liens');
+//console.log(aItems);
 
+var aItemsTop = [];
+
+    aItems.each(function(i){
+       var offsetTop = $(this).offset().top;
+//        console.log(offsetTop);
+       aItemsTop[i] = offsetTop;
+    });
+
+//$(aItems[0]).addClass('in-view');
+//
+        $(window).on('scroll', function(){
+//          console.log(aItems.slice(1));
+          itemInView(aItems.slice(1));
+          bgColour(aItemsTop);
+        });
+//
+        function itemInView(items) {
+          items.each(function(){
+            if (($(this).offset().top - $(window).scrollTop()) <= $(window).height()/2) {
+              $(this).addClass('in-view');
+            } else {
+              $(this).removeClass('in-view');
+            }
+          });
+        }
+
+        function bgColour(){
+          var curPos = $(window).scrollTop() + $(window).height() /2;
+//          console.log(curPos);
+          var _leng = $(aItems).length;
+          aItems.each(function(i){
+            if (curPos > $(aItems[i]).offset().top && i+1 == _leng || curPos > $(aItems[i]).offset().top && curPos < $(aItems[i+1]).offset().top) {
+              var itemClass = ($(this).attr('class'));
+              var theClass = itemClass.split(' ')[1];
+//              console.log(theClass);
+              $('.titre-actions.'+theClass).addClass('bg');
+              var currentaItemPos = $(aItems[i]).offset().top;
+              aItems.each(function(i){
+
+                  if($(aItems[i]).offset().top < currentaItemPos) {
+
+                      var itemClass = ($(this).attr('class'));
+                      var theClass = itemClass.split(' ')[1];
+
+                      $('.titre-actions.'+theClass).addClass('fill');
+                  }
+
+              });
+
+            } else {
+              var itemClass = ($(this).attr('class'));
+              var theClass = itemClass.split(' ')[1];
+
+              $('.titre-actions.'+theClass).removeClass('bg');
+              $('.titre-actions.'+theClass).removeClass('fill');
+            }
+          });
+        }
+    }
 };
 
 })(jQuery);
